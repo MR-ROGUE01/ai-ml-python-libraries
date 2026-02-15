8e7cbd9d836c4206d6169d4f932b19d871a71171
MR-ROGUE01 <rajamity011@gmail.com>
2026-02-15 13:29:06 +0530

    Daily Practice

---
+++ OS_MODULE/file_renamer.py
@@ -0,0 +1,11 @@
+import os
+
+os.chdir('C:/Users/ACER/Downloads/C_CODES')
+print(os.getcwd())
+
+for f in os.listdir():
+    file_name, file_ext = os.path.splitext(f)
+    file_name = file_name.strip().zfill(3)
+    new_name = file_name + file_ext
+    print(new_name)
+    os.rename(f,new_name)
